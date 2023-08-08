dfd785a1fd1e8d211a2bfecfca2ade609c5bda74
sunboquan <sunboquan@huawei.com>
2023-08-08 20:47:17 +0800

    License append

--- cluster_analyse/cluster_kernels_analysis/cluster_op_summary_analysis.py
+++ cluster_analyse/cluster_kernels_analysis/cluster_op_summary_analysis.py
@@ -1,7 +1,7 @@
-#!/bin/bash
-# Copyright 2023 Huawei Technologies Co., Ltd
+# Copyright (c) 2023, Huawei Technologies.
+# All rights reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0  (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 
 from pathlib import Path
 
