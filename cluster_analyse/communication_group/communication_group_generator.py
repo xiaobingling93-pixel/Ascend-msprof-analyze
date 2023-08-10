f348ca4e24dcb2090107a7597291e5ed5e9b5fed
sunboquan <sunboquan@huawei.com>
2023-08-10 09:58:35 +0800

    modify huawei company name in license

--- cluster_analyse/communication_group/communication_group_generator.py
+++ cluster_analyse/communication_group/communication_group_generator.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023, Huawei Technologies.
+# Copyright (c) 2023, Huawei Technologies Co., Ltd
 # All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0  (the "License");
@@ -13,20 +13,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+from common_func.constant import Constant
+from common_func.file_manager import FileManager
+
 class CommunicationGroupGenerator:
     def __init__(self, collection_path: str, data_map: dict):
         self.collection_path = collection_path
         self.data_map = data_map
         self.communication_group = {}
+        self.rank_comm_dir_dict = {}
 
     def generate(self):
-        pass
+        self.load_communication_json()
 
-    def read_communication_json(self):
-        pass
+    def load_communication_json(self):
+        for rank_id, profiling_dir_path in self.data_map:
+            comm_dir = profiling_dir_path.get(Constant.COMM_JSON)
+            if comm_dir:
+                self.rank_comm_dir_dict[rank_id] = FileManager.read_json_file(comm_dir)
+            if not self.rank_comm_dir_dict.get(rank_id):
+                print(f"rank {rank_id} does not have a valid communication.json")
+
 
     def generate_collective_communication_group(self):
         pass
 
     def generate_p2p_communication_group(self):
+        pass
+
+    def get_all_collective_ops_name(self):
         pass
