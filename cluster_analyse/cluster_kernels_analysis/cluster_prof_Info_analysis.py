c4a32518310895f3ce803ffa1115233ea036d29d
fanxiaotong <fanxiaotong32@163.com>
2023-09-27 11:52:58 +0800

    fix bug

--- cluster_analyse/cluster_kernels_analysis/cluster_prof_Info_analysis.py
+++ cluster_analyse/cluster_kernels_analysis/cluster_prof_Info_analysis.py
@@ -41,6 +41,8 @@ class FormDataProcessor:
         # 存储所有合并后的数据
         all_data = pd.DataFrame()
         for f in self.files:
+            if "":
+                continue
             # 读取CSV文件
             df = pd.read_csv(f)
             # 保留需要的列
