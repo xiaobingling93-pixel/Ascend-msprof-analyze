69608079109ac620361a18336b1523f50717e250
fanxiaotong <fanxiaotong32@163.com>
2023-09-27 16:32:02 +0800

    fix bug

--- cluster_analyse/cluster_kernels_analysis/cluster_prof_Info_analysis.py
+++ cluster_analyse/cluster_kernels_analysis/cluster_prof_Info_analysis.py
@@ -21,9 +21,8 @@ import re
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from plotly.offline import plot
-from threading import Thread
 import os
-import json
+
 import warnings
 
 
@@ -182,15 +181,9 @@ class StatisticalInfoToHtmlAnalyzer(OpSummaryAnalyzerBase):
         self.top_n = min(max(self.top_n, 1), len(view_data))
         top_n_data = view_data.sort_values(("Task Duration(us)", 'var'), ascending=False).head(self.top_n)
 
-        threads = []
         for column in self.columns_to_view:
             # 分别给每一种特性画图
-            draw_thread = Thread(target=self.drawPloty, args=(column, summary_data, top_n_data, rank_num))
-            threads.append(draw_thread)
-            draw_thread.start()
-
-        for draw_thread in threads:
-            draw_thread.join()
+            self.drawPloty(column, summary_data, top_n_data, rank_num)
 
     def drawPloty(self, column, summary_data, top_n_data, rank_num):
         col_num = self.getCalNum(rank_num)
