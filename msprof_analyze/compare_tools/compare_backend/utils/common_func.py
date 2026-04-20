127b21c05a4f4629121d5fbfdeed11303ab6bfc0
xfeng <zchlcw663@163.com>
2026-04-20 11:43:33 +0800

    最长公共子序列方法性能优化

--- msprof_analyze/compare_tools/compare_backend/utils/common_func.py
+++ msprof_analyze/compare_tools/compare_backend/utils/common_func.py
@@ -13,9 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from decimal import Decimal
-
-import numpy as np
-
+from difflib import SequenceMatcher
 from msprof_analyze.prof_common.logger import get_logger
 
 logger = get_logger()
@@ -53,66 +51,31 @@ def convert_to_decimal(data: any) -> Decimal:
     return decimal_value
 
 
-def longest_common_subsequence_matching(base_ops: list, comparison_ops: list, name_func: any) -> list:
-    if not comparison_ops:
-        return [[value, None] for value in base_ops]
-    if not base_ops:
-        return [[None, value] for value in comparison_ops]
-
-    comparison_len, base_len = len(comparison_ops), len(base_ops)
-    if comparison_len * base_len > 50 * 10 ** 8:
-        logger.warning('The comparison time is expected to exceed 30 minutes, if you want to see the results quickly, '
-                       'you can restart comparison task and turn on the switch --disable_details.')
-
-    pre_list = np.zeros(base_len + 1, dtype=np.int32)
-    cur_list = np.zeros(base_len + 1, dtype=np.int32)
-
-    all_base_data = [hash(name_func(op)) for op in base_ops]
-    all_comparison_data = [hash(name_func(op)) for op in comparison_ops]
-
-    dp_flag = BitMap((comparison_len + 1) * (base_len + 1))  # flag for only comparison op
-
-    comparison_index = 1
-    for comparison_data in iter(all_comparison_data):
-        base_index = 1
-        for base_data in all_base_data:
-            if comparison_data == base_data:
-                cur_list[base_index] = pre_list[base_index - 1] + 1
-            else:
-                only_base = cur_list[base_index - 1]
-                only_comparison = pre_list[base_index]
-                if only_base < only_comparison:
-                    dp_flag.add(comparison_index * base_len + base_index)
-                    cur_list[base_index] = only_comparison
-                else:
-                    cur_list[base_index] = only_base
-            base_index += 1
-        pre_list = cur_list
-        comparison_index += 1
-
-    matched_op = []
-    comparison_index, base_index = comparison_len, base_len
-    while comparison_index > 0 and base_index > 0:
-        base_data = base_ops[base_index - 1]
-        comparison_data = comparison_ops[comparison_index - 1]
-        if all_base_data[base_index - 1] == all_comparison_data[comparison_index - 1]:
-            matched_op.append([base_data, comparison_data])
-            comparison_index -= 1
-            base_index -= 1
-        elif (comparison_index * base_len + base_index) in dp_flag:
-            matched_op.append([None, comparison_data])
-            comparison_index -= 1
-        else:
-            matched_op.append([base_data, None])
-            base_index -= 1
-    while comparison_index > 0:
-        matched_op.append([None, comparison_ops[comparison_index - 1]])
-        comparison_index -= 1
-    while base_index > 0:
-        matched_op.append([base_ops[base_index - 1], None])
-        base_index -= 1
-    matched_op.reverse()
-    return matched_op
+def longest_common_subsequence_matching(base_ops, comparison_ops, name_func):
+    base_sequence = [name_func(op) for op in base_ops]
+    comparison_sequence = [name_func(op) for op in comparison_ops]
+    matcher = SequenceMatcher(None, base_sequence, comparison_sequence, autojunk=False)
+    result = []
+    i = j = 0
+    for block in matcher.get_matching_blocks():
+        while i < block.a:
+            result.append([base_ops[i], None])
+            i += 1
+        while j < block.b:
+            result.append([None, comparison_ops[j]])
+            j += 1
+        match_len = block.size
+        for k in range(match_len):
+            result.append([base_ops[i + k], comparison_ops[j + k]])
+        i += match_len
+        j += match_len
+    while i < len(base_ops):
+        result.append([base_ops[i], None])
+        i += 1
+    while j < len(comparison_ops):
+        result.append([None, comparison_ops[j]])
+        j += 1
+    return result
 
 
 class BitMap:
